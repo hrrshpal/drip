d1460d781ff254bc2a032df57497a41fa0f1b9fe
harsh <hrrshpal@gmail.com>
2026-03-23 11:36:16 +0000

    finished home and collection module

---
+++ frontend/src/context/ShopContext.jsx
@@ -0,0 +1,24 @@
+import { createContext } from "react";
+import { products } from "../assets/frontend_assets/assets";
+
+export const ShopContext = createContext();
+
+const ShopContextProvider = (props) => {
+    const currency = '₹';
+    const deliveryFee = 10;
+    const [search, setSearch] = useState('');
+    const [showSearch, setShowSearch] = useState(true)
+    
+    const value = {
+        products, currency, deliveryFee,
+        search, setSearch, showSearch, setShowSearch
+    }
+
+    return(
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider;
