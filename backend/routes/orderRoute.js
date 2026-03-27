6959dd3bc14fe41af8a775aa42cb92947aff0e10
harsh <hrrshpal@gmail.com>
2026-03-27 13:49:51 +0000

    created place order feature

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import { allOrders, placeOrder, placeOrderRazorpay, placeOrderStripe, updateOrderStatus, userOrders } from '../controller/orderController.js'
+import adminAuth from '../middleware/adminAuth.js'
+import authUser from '../middleware/auth.js'
+
+const orderRouter = express.Router()
+
+//Admin features
+orderRouter.post('/list', adminAuth, allOrders)
+orderRouter.post('/status', adminAuth, updateOrderStatus)
+
+//Payment features
+orderRouter.post('/place', authUser, placeOrder)
+orderRouter.post('/stripe', authUser, placeOrderStripe)
+orderRouter.post('/razorpay', authUser, placeOrderRazorpay)
+
+//User feature
+orderRouter.post('/userorders', authUser, userOrders)
+
+export default orderRouter
